c2b18df6cd7a89d25cdcd68420eb3857d6a0ad53
rifaldi34 <rifaldisetiawan6@gmail.com>
2020-06-15 09:59:56 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Konfigurasi untuk situs
 
-	$instalasi = 'http://127.0.0.1/pak_jabbar/resensi_film'; //Ini adalah alamat instalasi
+	$instalasi = 'http://127.0.0.1/pak_jabbar/resensi_film'; //Ini adalah alamat instalasi, tanpa tanda '/' di akhir alamat
 	$blur1 = true; //Ini untuk mengaktifkan/menonaktifkan efek blur pada navbar & halaman depan
 ?>
